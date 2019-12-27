ba07ce3291727b9269421a60de73fd9da6d718f2
paynege <4012908@qq.com>
2019-12-27 15:44:01 +0800

    implement output template file func

--- main.go
+++ main.go
@@ -8,17 +8,28 @@ import (
 )
 
 type test struct {
-	Name string
+	Name  string
 	Value string
 }
 
 func main() {
 	t := test{"Hello", "World!"}
+	s := `192.168.1.100,
+192.168.1.105,
+192.168.1.102,
+192.168.1.103`
+	t.Value = s
 	tpl, err := template.New("test.tpl").ParseFiles("test.tpl")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = tpl.Execute(os.Stdout, t)
+
+	tpFile, err := os.Create("test.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tpl.Execute(tpFile, t)
+
 	if err != nil {
 		log.Fatalln(err)
 	}
